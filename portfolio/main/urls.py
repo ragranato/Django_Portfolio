036ff716bd0b934824b3835049147f020afb00e0
ragranato <robert.granato@gmail.com>
2024-05-20 23:01:53 -0400

    virtual env

--- portfolio/main/urls.py
+++ portfolio/main/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("home/", views.home, name="home")
-    path("contact/", views.contact, name="contact")
+    path("home/", views.home, name="home"),
+    path("contact/", views.contact, name="contact"),
     path("project/<int:id>/", views.project, name="project")
 ]
